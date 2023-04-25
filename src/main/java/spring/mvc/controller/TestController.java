7533009aead7cf848cd600865e8f1da4841c0525
BoHyun Choi <126458406+lambdaramge@users.noreply.github.com>
2023-04-25 09:54:24 +0900

    테스트 받아보기

---
+++ src/main/java/spring/mvc/controller/TestController.java
@@ -0,0 +1,28 @@
+package spring.mvc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import spring.mvc.dto.TestDto;
+import spring.mvc.service.TestService;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	TestService service;
+
+	@GetMapping("/test")
+	public String test(Model model) {
+		
+		List<TestDto> list=service.selectData();
+		
+		model.addAttribute("list", list);
+		
+		return "/test/hi";
+	}
+}
